155ee53739cfc105266d5dae92fcbafbcae71e55
romankolosok <romakolosok14@gmail.com>
2023-02-24 09:51:16 -0500

    created like script for comments

--- scripts/comment.js
+++ scripts/comment.js
@@ -1,5 +1,19 @@
-let increment = document.querySelector('.counter')
+function likeIncrement(element) {
+    elementParent = element.parentNode
+    parentsElementParent = elementParent.parentNode
+    oppositeValue = -1 * Number(element.getAttribute('value'))
+    ownCounter = elementParent.querySelector('.counter')
+    oppositeCounter = parentsElementParent.querySelector(`label:has([value='${oppositeValue}']) .counter`)
+    btn = parentsElementParent.querySelector(`[value='${oppositeValue}']`)
+    if(btn.active){
+        btn.active = false
+        counterValue = oppositeCounter.textContent
+        oppositeCounter.textContent = +counterValue - 1
+    }
+    if(!element.active) {
+        element.active = true
+        counterValue = ownCounter.textContent
+        ownCounter.textContent = Number(counterValue) + 1
+    }
 
-increment.addEventListener('click', (event) => {
-    console.log(increment)
-})
+}
